6f9f48a07dd4f2a1173d1581f226f297e7d83e07
S.K Shuvo <zamiulbasedshuvo444@gmail.com>
2023-10-24 01:53:26 +0600

    management section added

--- src/Pages/Carousel/Carousel.jsx
+++ src/Pages/Carousel/Carousel.jsx
@@ -3,17 +3,19 @@ import img1 from '../../assets/noman30.jpg'
 import img2 from '../../assets/noman16.jpg'
 import img3 from '../../assets/noman19.jpg'
 import img4 from '../../assets/noman22.jpg'
+import { FaArrowRight } from "react-icons/fa6";
+import Management from '../Management/Management';
 
 const Content = () => {
     return (
         <div className='w-full'>
-<div className="carousel w-full min-h-screen " >
-  <div id="slide1" className="carousel-item relative w-full " style={{backgroundImage:`linear-gradient(to left, rgb(23 59 149 / 10%), rgb(5 19 113 / 73%)), url(${img1}), linear-gradient(to right, rgba(0, 224, 255, 1), rgba(0, 133, 255, 1)) `, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover', marginTop: '-9%', opacity:'1'}} >
+<div className="carousel w-full min-h-screen" >
+  <div id="slide1" className="carousel-item relative w-full " style={{backgroundImage:`linear-gradient(to left, rgba(23, 59, 149, 0.1), rgb(0 55 46)), url(${img1}), linear-gradient(to right, rgba(0, 224, 255, 1), rgba(0, 133, 255, 1)) `, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover', marginTop: '-9%', opacity:'1'}} >
   {/* <div className='bg-gradient-to-r from-violet-500 to-fuchsia-500'> */}
-  <div className=" max-w-md mt-96 ms-24 text-white ">
+  <div className=" max-w-md mt-96 ms-48 text-white ">
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-secondary">Our Portfolio <FaArrowRight></FaArrowRight></button>
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
@@ -21,33 +23,33 @@ const Content = () => {
     {/* </div> */}
   </div>
   </div>
-  <div id="slide2" className="carousel-item relative w-full" style={{backgroundImage:`linear-gradient(to left, rgb(23 59 149 / 10%), rgb(5 19 113 / 73%)), url(${img2})`, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover',marginTop: '-9%'}} >
-  <div className="max-w-md mt-96 ms-24 text-white">
+  <div id="slide2" className="carousel-item relative w-full" style={{backgroundImage:`linear-gradient(to left, rgba(23, 59, 149, 0.1), rgb(0 55 46)), url(${img2})`, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover',marginTop: '-9%'}} >
+  <div className="max-w-md mt-96 ms-48 text-white">
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-secondary">Our Portfolio <FaArrowRight></FaArrowRight></button>
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a> 
     </div>
   </div>
-  <div id="slide3" className="carousel-item relative w-full" style={{backgroundImage:`linear-gradient(to left, rgb(23 59 149 / 10%), rgb(5 19 113 / 73%)), url(${img3})`, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover', marginTop: '-9%'}} >
-  <div className="max-w-md mt-96 ms-24 text-white">
+  <div id="slide3" className="carousel-item relative w-full" style={{backgroundImage:`linear-gradient(to left, rgba(23, 59, 149, 0.1), rgb(0 55 46)), url(${img3})`, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover', marginTop: '-9%'}} >
+  <div className="max-w-md mt-96 ms-48 text-white">
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-secondary">Our Portfolio <FaArrowRight></FaArrowRight></button>
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a> 
     </div>
   </div>
-  <div id="slide4" className="carousel-item relative w-full" style={{backgroundImage:`linear-gradient(to left, rgb(23 59 149 / 10%), rgb(5 19 113 / 73%)), url(${img4})`, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover', marginTop: '-9%'}} >
-  <div className="max-w-md mt-96 ms-24 text-white">
+  <div id="slide4" className="carousel-item relative w-full" style={{backgroundImage:`linear-gradient(to left,rgba(23, 59, 149, 0.1), rgb(0 55 46)), url(${img4})`, backgroundRepeat:'no-repeat', width:'100vw' , backgroundSize: 'cover', marginTop: '-9%'}} >
+  <div className="max-w-md mt-96 ms-48 text-white">
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-secondary mb-8">Our Portfolio <FaArrowRight></FaArrowRight></button>
     </div>
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
@@ -55,6 +57,9 @@ const Content = () => {
     </div>
   </div>
  
+  </div>
+  <div>
+  <Management></Management>
   </div>
         </div>
     );
---
+++ src/Pages/Management/Management.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import img1 from '../../assets/person6.jpg'
+import img2 from '../../assets/person7.jpg'
+import img3 from '../../assets/person8.jpg'
+import img4 from '../../assets/person9.jpg'
+import { FaArrowRight } from "react-icons/fa6";
+import Projects from '../Project/Projects';
+
+const Management = () => {
+    return (
+        <div>
+            <div className=' bg-pink-50'>
+
+<h2 className=' font-bold text-center mb-8 pt-8 text-2xl'>Our <span className='text-red-500'> Management</span> Team</h2>
+
+<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-8'>
+    <div className="card max-w-sm mx-auto  bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10 ">
+            <img src={img1} alt="Shoes" className="h-36 rounded-full" />
+        </figure>
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">Chairman,Noman Group</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi, autem.</p>
+        </div>
+    </div>
+    <div className="card max-w-sm mx-auto  bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10 ">
+            <img src={img2} alt="Shoes" className="h-36 rounded-full w-48" />
+        </figure>
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">ED, NWML</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, nulla!</p>
+        </div>
+    </div>
+    <div className="card max-w-sm mx-auto bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10 ">
+            <img src={img3} alt="Shoes" className="h-36 rounded-full w-48" />
+        </figure>
+        <div className="card-body items-center text-center">
+            <h2 className="card-title">DGM, NWML</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, excepturi?</p>
+        </div>
+    </div>
+    <div className="card max-w-sm mx-auto  bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10 ">
+            <img src={img4} alt="Shoes" className="h-36 rounded-full w-48" />
+        </figure>
+        <div className="card-body items-center text-center">
+            <h2 className="card-title"> Admin Manager</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, atque.</p>
+
+        </div>
+    </div>
+</div>
+
+
+<div className='mt-6 text-center'>
+    <button className="btn btn-outline btn-secondary my-8">See All <FaArrowRight></FaArrowRight></button>
+</div>
+
+</div>
+<div>
+    <Projects></Projects>
+</div>
+        </div>
+    );
+};
+
+export default Management;
